a93a298969785c9679113c81afc7ab579235615a
XueZhang <xue.zhang@aayongche.com>
2018-03-26 14:48:20 +0800

    2018-3-26星期一第二次提交

--- app/src/main/java/com/example/sweet_xue/gitdeconddemo/MainActivity.java
+++ app/src/main/java/com/example/sweet_xue/gitdeconddemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                Toast.makeText()
 
             }
         });
