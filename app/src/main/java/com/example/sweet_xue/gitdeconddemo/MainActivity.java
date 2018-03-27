b16a96399ce8d45deb27a98db12a779287d0d8a5
XueZhang <xue.zhang@aayongche.com>
2018-03-27 17:22:42 +0800

    2018-3-27星期二第一次提交

--- app/src/main/java/com/example/sweet_xue/gitdeconddemo/MainActivity.java
+++ app/src/main/java/com/example/sweet_xue/gitdeconddemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sweet_xue.gitdeconddemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,12 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int type = WindowManager.LayoutParams.TYPE_BASE_APPLICATION;
+
+
         tv = findViewById(R.id.tv_main);
         tv.setText("this is textview");
         tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Toast.makeText()
+                Toast.makeText(MainActivity.this,"",Toast.LENGTH_SHORT).show();
 
             }
         });
